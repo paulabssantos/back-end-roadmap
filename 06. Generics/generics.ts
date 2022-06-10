f0f696d5dcd919c6bffa13aabd8b11ce5d10b057
paulabssantos <paulablsantos7@gmail.com>
2022-06-10 08:24:54 -0400

    refactor: alterando a assinatura do tipo useState no Generics

--- 06. Generics/generics.ts
+++ 06. Generics/generics.ts
@@ -4,7 +4,7 @@
 //V -> Value
 //E -> Element
 
-function useState <S>(){
+function useState <S extends number | string>(){
     let state: S
 
     function getState(){
@@ -13,5 +13,15 @@ function useState <S>(){
    
     function setState(newState: S){
         state = newState
-    }
+    }   
+
+    return {getState, setState}
 }
+
+const newState = useState()
+
+newState.setState("foo") //no momento que foi definido que é uma string, não é possível submeter valor de tipo diferente
+console.log(newState.getState())
+
+newState.setState(1) //erro, pois string já foi definido na linha 23
+console.log(newState.getState())
